95dd52573aeb36678cb60b3b198ab84a6e8acc33
kenkokan <kenkokan@mail.ru>
2025-03-24 14:09:02 +0300

    After adding fixtures for goods

--- main/views.py
+++ main/views.py
@@ -1,13 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from goods.models import Categories
 # Create your views here.
 
 def index(request):
+
+    categories = Categories.objects.all()
+
     context: dict[str, str] = {
         'title': 'ИМПЕРИЯРЕМОНТА.РФ',
-        'content': 'ИМПЕРИЯРЕМОНТА.РФ'
-
+        'content': 'ИМПЕРИЯРЕМОНТА.РФ',
+        'categories': categories
     }
+
     return render(request, 'main/index.html', context)
 
 
