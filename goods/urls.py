7fa2ca766832dc46d305e2463fe9b2eaf97eb872
kenkokan <kenkokan@mail.ru>
2025-04-12 13:07:34 +0300

    Slug convert for categories

--- goods/urls.py
+++ goods/urls.py
@@ -1,3 +1,4 @@
+from itertools import product
 from django.urls import path
 from goods import views
 
@@ -5,7 +6,7 @@ from goods import views
 app_name = 'goods'
 
 urlpatterns = [
-    path('', views.catalog, name='index'),
-    path('product/<slug:product_slug>/', views.product, name='product'),
-    
+    path('<slug:category_slug>/', views.catalog, name='index'),
+    # path('product/<int:product_id>/', views.product, name='product'),
+    path('product/<slug:product_slug>/', views.product, name='product'),   
 ]
